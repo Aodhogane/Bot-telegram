5780a30cb22b1bff1371b8d737d4e5361fb03289
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-10-14 01:10:29 +0300

    Добавление боту ограничение от спама экспонатми.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ bot = telebot.TeleBot(Token)
 
 # Словарь для отслеживания состояния выбранного зала для каждого пользователя
 user_state = {}
+processing_exhibit = {}  # Новый словарь для отслеживания состояния обработки экспоната
 
 # Старт
 @bot.message_handler(commands=['start'])
@@ -15,6 +16,7 @@ def start(message):
     keyboard = types.ReplyKeyboardMarkup()
     keyboard.add(types.KeyboardButton('Зал №1'))
     keyboard.add(types.KeyboardButton('Зал №2'))
+    keyboard.add(types.KeyboardButton('Зал №3'))
     bot.send_message(message.chat.id, 'Введите номер экспоната, который вас интересует', reply_markup=keyboard)
 
 # Обработка сообщений
@@ -23,25 +25,15 @@ def hall_selection(message):
     global user_state
     
     # Обработка выбора зала
-    if message.text == 'Зал №1':
-        user_state[message.chat.id] = 'Зал №1'
+    if message.text in ['Зал №1', 'Зал №2', 'Зал №3']:
+        user_state[message.chat.id] = message.text
         keyboard = types.ReplyKeyboardMarkup()
         keyboard.add(types.KeyboardButton('Экспонат №1'))
         keyboard.add(types.KeyboardButton('Экспонат №2'))
         keyboard.add(types.KeyboardButton('Экспонат №3'))
         keyboard.add(types.KeyboardButton('Экспонат №4'))
         keyboard.add(types.KeyboardButton('Назад'))
-        bot.send_message(message.chat.id, 'Выберите номер экспоната, который вас интересует в "Зал №1"', reply_markup=keyboard)
-
-    elif message.text == 'Зал №2':
-        user_state[message.chat.id] = 'Зал №2'
-        keyboard = types.ReplyKeyboardMarkup()
-        keyboard.add(types.KeyboardButton('Экспонат №1'))
-        keyboard.add(types.KeyboardButton('Экспонат №2'))
-        keyboard.add(types.KeyboardButton('Экспонат №3'))
-        keyboard.add(types.KeyboardButton('Экспонат №4'))
-        keyboard.add(types.KeyboardButton('Назад'))
-        bot.send_message(message.chat.id, 'Выберите номер экспоната, который вас интересует в "Зал №2"', reply_markup=keyboard)
+        bot.send_message(message.chat.id, f'Выберите номер экспоната, который вас интересует в "{message.text}"', reply_markup=keyboard)
 
     elif message.text == 'Назад':
         start(message)
@@ -50,7 +42,11 @@ def hall_selection(message):
     elif message.text.startswith('Экспонат №'):
         current_hall = user_state.get(message.chat.id)
         if current_hall:
-            handle_exhibit(message, current_hall)
+            if processing_exhibit.get(message.chat.id, False):
+                bot.send_message(message.chat.id, 'Пожалуйста, дождитесь обработки вашего выбора экспоната.')
+            else:
+                processing_exhibit[message.chat.id] = True  # Устанавливаем флаг обработки
+                handle_exhibit(message, current_hall)
 
 def handle_exhibit(message, hall):
     exhibit_info = {
@@ -65,6 +61,12 @@ def handle_exhibit(message, hall):
             'Экспонат №2': "Экспонат №2, Зал №2. Иван Четвёртый Васильевич родился 1530 умер 1584 — государь, царь и великий князь всея Руси с 1533 года, первый венчанный царь всея Руси с 1547 года.",
             'Экспонат №3': "Экспонат №3, Зал №2. Подарок турецкого султана Абдул-Хамида Первого, императрице Екатерине II в 1775 году. Представляет собой украшение для лошади, инкрустированное дорогим сапфиром и покрытое серебром.",
             'Экспонат №4': "Экспонат №4, Зал №2. Учебник, который был написан великим учёным нашего времени."
+        },
+        'Зал №3': {
+            'Экспонат №1': "Экспонат №1, Зал №3. Картина Пётра I Алексе́евича, Пётр Вели́кий родился 30 мая 1672, Москва, умер 28 января 1725, в Санкт-Петербурге — царь всея Руси с 1682 года, первый император Всероссийский с 1721 года.",
+            'Экспонат №2': "Экспонат №2, Зал №3. Иван Четвёртый Васильевич родился 1530 умер 1584 — государь, царь и великий князь всея Руси с 1533 года, первый венчанный царь всея Руси с 1547 года.",
+            'Экспонат №3': "Экспонат №3, Зал №3. Подарок турецкого султана Абдул-Хамида Первого, императрице Екатерине II в 1775 году. Представляет собой украшение для лошади, инкрустированное дорогим сапфиром и покрытое серебром.",
+            'Экспонат №4': "Экспонат №4, Зал №3. Учебник, который был написан великим учёным нашего времени."
         }
     }
 
@@ -77,5 +79,8 @@ def handle_exhibit(message, hall):
     finally:
         file.close()
         os.remove(f"{message.from_user.id}.mp3")
+    
+    # Сбрасываем флаг обработки после завершения
+    processing_exhibit[message.chat.id] = False
 
 bot.polling(none_stop=True)
